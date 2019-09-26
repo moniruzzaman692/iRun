f6cd599b0bf35073a2ca5ce4122519bb3d90c38c
moniruzzaman692 <55833280+moniruzzaman692@users.noreply.github.com>
2019-09-26 15:55:46 +0600

    Initial project open code  by Akash

---
+++ iRun/iRun/iRun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iRun
+//
+//  Created by kuet on 26/9/19.
+//  Copyright © 2019 kuet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
